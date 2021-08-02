10f8229b7a37487881c961f6fd9b2e7baaf443b5
laxative.hsieh <laxative.hsieh@yi-spaces.com>
2021-08-02 14:24:07 +0800

    Feat(UserPanel): implement appearance

--- src/components/UserPanel.vue
+++ src/components/UserPanel.vue
@@ -1,5 +1,19 @@
 <template lang="pug">
-#user-panel
+aside#user-panel
+  .avatar-wrapper
+    .avatar
+  nav.nav-bar
+    a.link-button
+      img(src="../assets/svg/userPanel/home-52.svg")
+    a.link-button
+      img(src="../assets/svg/userPanel/ic_library_music_48px.svg")
+    a.link-button
+      img(src="../assets/svg/userPanel/archive-2.svg")
+  .setting-bar
+    a.link-button
+      img(src="../assets/svg/userPanel/zoom-2.svg")
+    a.link-button
+      img(src="../assets/svg/userPanel/settings-gear-63.svg")
 </template>
 
 <script lang="ts">
@@ -19,5 +33,49 @@ export default defineComponent({
   right: 0;
 
   background-color: #4b3434;
+}
+
+.avatar-wrapper {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .avatar {
+    width: 56px;
+    height: 56px;
+    background-color: #ffffff34;
+    border: 1px solid #707070;
+    border-radius: 50%;
+  }
+}
+
+.nav-bar {
+  width: 100%;
+  height: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.setting-bar {
+  width: 100%;
+  height: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.link-button {
+  margin: 25px 0;
+  opacity: 0.6;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    opacity: 1;
+  }
 }
 </style>
